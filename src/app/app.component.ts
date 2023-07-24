ffed3afe7d628c46dd406ee93ad5d7dfd45356d4
Ryan Tate <Ryan.Tate@ClaytonHomes.com>
2023-07-24 16:41:35 -0400

    Add Osano and Test Async Script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    console.log("Including run-time dependency.");
+
+    var o = document.createElement("script");
+    o.async = true;
+    o.src = 'assets/test.js';
+    o.onload = function(event)
+    {
+      // Should only be fired once.
+      console.log("OnloadEvent Fired", event);
+    };
+    document.querySelector("head")!.appendChild(o);
+
+  }
+
   title = 'osano-test';
 }
